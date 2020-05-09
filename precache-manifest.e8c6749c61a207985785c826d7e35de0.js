dcf5a3d6e2c52419e437fb276fc0bdc2df4d8b65
yuvaraj <yuvaraj@test.com>
2020-05-09 23:59:53 +0530

    Updates

---
+++ precache-manifest.e8c6749c61a207985785c826d7e35de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb12b4319ae07a20f830977074675cd",
+    "url": "/react-basic-crud-ops/index.html"
+  },
+  {
+    "revision": "8d53879fd9d0eb69d274",
+    "url": "/react-basic-crud-ops/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cef19d2e3f2655038f35",
+    "url": "/react-basic-crud-ops/static/js/2.6205a552.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-crud-ops/static/js/2.6205a552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d53879fd9d0eb69d274",
+    "url": "/react-basic-crud-ops/static/js/main.c5f01ffb.chunk.js"
+  },
+  {
+    "revision": "b3682d22511b50fa5f1f",
+    "url": "/react-basic-crud-ops/static/js/runtime-main.c03a5159.js"
+  }
+]);
